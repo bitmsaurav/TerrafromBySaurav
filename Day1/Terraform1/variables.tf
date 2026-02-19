82cb0ca60e146d521a4af38676902f600d845925
bitmsaurav <bitmsaurav@gmail.com>
2026-02-19 09:11:01 +0530

    Input variable/Assigen the value from the cli/promptvalue

---
+++ Day1/Terraform1/variables.tf
@@ -0,0 +1,41 @@
+#Input variables
+
+# 1. Business Unit name
+variable "business_unit" {
+  description = "This is a Bussiness Unit Name"
+  type        = string
+  default     = "hr"
+}
+
+#2. Envirnoment Name
+
+variable "envitnoment" {
+  description = "This is the Environament Name"
+  type        = string
+  default     = "dev"
+
+}
+
+#3 . Resoure Group name
+
+variable "resource_group_name" {
+  description = "this is the resource group name"
+  type        = string
+  default     = "myrg"
+}
+
+#4. Resource location name
+variable "resource_location_name" {
+  description = "this is the resource location name"
+  type        = string
+  default     = "centralindia"
+
+}
+
+#5.variable for network name
+variable "virtual_network_name" {
+  description = "This the Network name"
+  type        = string
+  default     = "myvnet"
+
+}
